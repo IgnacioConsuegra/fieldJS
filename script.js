fbb9e997967dd5bbfcbbbe1675a2e6e3d3ab47df
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2024-03-29 20:24:46 +0100

    It movesclear

--- script.js
+++ script.js
@@ -32,19 +32,35 @@ class FlowFieldEffect{
   constructor(ctx, width, height) {
     this.#ctx = ctx;
     this.#ctx.strokeStyle = 'white';
-    this.#ctx.lineWidth = 5;
+    this.#ctx.lineWidth = 1;
     this.#width = width;
     this.#height = height;
     this.lastTime = 0;
     this.interval = 1000/60;
     this.timer = 0;
     this.cellSize = 15;
+    this.gradient;
+    this.#createGradient();
+    this.#ctx.strokeStyle = this.gradient;
+    this.radius = 0;
+    this.vr = 0.03;
   }
-  #drawLine(x, y) {
+  #createGradient() {
+    this.gradient = this.#ctx.createLinearGradient(0, 0, this.#width, this.#height);
+    this.gradient.addColorStop("0.1", "#ff5c33");
+    this.gradient.addColorStop("0.2", "#ff66b3");
+    this.gradient.addColorStop("0.4", "#ccccff");
+    this.gradient.addColorStop("0.6", "#b3ffff");
+    this.gradient.addColorStop("0.8", "#80ff80");
+    this.gradient.addColorStop("0.9", "#ffff33");
+  }
+  #drawLine(angle, x, y) {
     const length = 300;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(x + 5, y + 5);
+    this.#ctx.lineTo(
+      x + Math.cos(angle) * 30, y + Math.sin(angle) * 30 
+      );
     this.#ctx.stroke();
   }
   animate(timeStamp) {
@@ -52,9 +68,13 @@ class FlowFieldEffect{
     this.lastTime = timeStamp;
     if (this.timer > this.interval) {
       this.#ctx.clearRect(0, 0, this.#width, this.#height);
+      this.radius += this.vr;
       for (let y = 0; y < this.#height; y += this.cellSize) {
         for (let x = 0; x < this.#width; x += this.cellSize) {
-          this.#drawLine(x, y);
+          const angle = 
+          (Math.cos(x * 0.01) + 
+          Math.sin(y * 0.01)) * this.radius;
+          this.#drawLine(angle, x, y);
         }
       }
       this.timer = 0;
