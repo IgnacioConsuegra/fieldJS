1e32f29a2eeb909b481c7b992871a97b714a949d
Ignacio Consuegra <ignacioandresconsuegradelacruz@gmail.com>
2024-03-29 19:25:43 +0100

    created a lots of points.

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ window.onload = function() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-  flowField.animate(); 
+  flowField.animate(0); 
 }
 window.addEventListener('resize', function(){
   cancelAnimationFrame(flowFieldAnimation)
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   flowField = new FlowFieldEffect(ctx, canvas.width, canvas.height);
-  flowField.animate(); 
+  flowField.animate(0); 
 })
 const mouse = {
   x: 0,
@@ -24,8 +24,6 @@ const mouse = {
 window.addEventListener('mousemove', function(e){
   mouse.x = e.x;
   mouse.y = e.y;
-  console.log(mouse.x);
-  console.log(mouse.y);
 })
 class FlowFieldEffect{
   #ctx;
@@ -34,25 +32,36 @@ class FlowFieldEffect{
   constructor(ctx, width, height) {
     this.#ctx = ctx;
     this.#ctx.strokeStyle = 'white';
+    this.#ctx.lineWidth = 5;
     this.#width = width;
     this.#height = height;
-    this.angle = 0;
+    this.lastTime = 0;
+    this.interval = 1000/60;
+    this.timer = 0;
+    this.cellSize = 15;
   }
-  #draw(x, y) {
+  #drawLine(x, y) {
     const length = 300;
     this.#ctx.beginPath();
     this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(mouse.x, mouse.y);
+    this.#ctx.lineTo(x + 5, y + 5);
     this.#ctx.stroke();
   }
-  animate() {
-    this.angle += 0.1;
-    this.#ctx.clearRect(0, 0, this.#width, this.#height)
-    this.#draw(
-      this.#width / 2 + Math.sin(this.angle) * 100,
-      this.#height / 2 + Math.cos(this.angle) * 100,
-    );
-    console.log("Animating.");
+  animate(timeStamp) {
+    const deltaTime = timeStamp - this.lastTime;
+    this.lastTime = timeStamp;
+    if (this.timer > this.interval) {
+      this.#ctx.clearRect(0, 0, this.#width, this.#height);
+      for (let y = 0; y < this.#height; y += this.cellSize) {
+        for (let x = 0; x < this.#width; x += this.cellSize) {
+          this.#drawLine(x, y);
+        }
+      }
+      this.timer = 0;
+    } else {
+      this.timer += deltaTime;
+    }
+    
     flowFieldAnimation = requestAnimationFrame(this.animate.bind(this))
   }
 }
